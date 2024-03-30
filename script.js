7e6774d889846a1aa5f964cfe28c813a8d9faf1a
“lloyd3126” <lloyd3126@gmail.com>
2024-03-31 01:20:40 +0800

    add more options to adjust font

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+$(document).ready(function() {
+    $('#fontSizeInput').on('input', function() {
+    let fontSizeInput = $("#fontSizeInput").val();
+    let letterSpacingInput = $("#letterSpacingInput").val();
+    let lineHeightInput = $("#lineHeightInput").val();
+    $('#capture').css('fontSize', fontSizeInput + 'px');
+    $('#capture').css('letterSpacing', letterSpacingInput + 'px');
+    $('#capture').css('lineHeight', lineHeightInput);
+  });
+  $('#letterSpacingInput').on('input', function() {
+     let fontSizeInput = $("#fontSizeInput").val();
+    let letterSpacingInput = $("#letterSpacingInput").val();
+    let lineHeightInput = $("#lineHeightInput").val();
+    $('#capture').css('fontSize', fontSizeInput + 'px');
+    $('#capture').css('letterSpacing', letterSpacingInput + 'px');
+    $('#capture').css('lineHeight', lineHeightInput);
+  });
+  $('#lineHeightInput').on('input', function() {
+     let fontSizeInput = $("#fontSizeInput").val();
+    let letterSpacingInput = $("#letterSpacingInput").val();
+    let lineHeightInput = $("#lineHeightInput").val();
+    $('#capture').css('fontSize', fontSizeInput + 'px');
+    $('#capture').css('letterSpacing', letterSpacingInput + 'px');
+    $('#capture').css('lineHeight', lineHeightInput);
+  });
+});
+
 $('#mytextarea').tinymce({
   content_style:
     "body {font-size: 24px; line-height: 0.25;}",
   menubar: false,
   branding: false,
-  toolbar: false,
+  toolbar: 'forecolor',
   height: 240,
   placeholder: '請輸入文字 ...',
   setup: function (editor) {
@@ -12,13 +39,29 @@ $('#mytextarea').tinymce({
       console.log(selectedContent);
       $("#capture").html(selectedContent);
       if(!selectedContent) $("#capture").html("<p>請輸入文字 ...</p>");
+      let fontSizeInput = $("#fontSizeInput").val();
+      let letterSpacingInput = $("#letterSpacingInput").val();
+      let lineHeightInput = $("#lineHeightInput").val();
+      $('#capture').css('fontSize', fontSizeInput + 'px');
+      $('#capture').css('letterSpacing', letterSpacingInput + 'px');
+      $('#capture').css('lineHeight', lineHeightInput);
+    });
+    editor.on('change', function() {
+      let selectedContent = editor.getContent();
+      console.log(selectedContent);
+      $("#capture").html(selectedContent);
+      if(!selectedContent) $("#capture").html("<p>請輸入文字 ...</p>");
+      let fontSizeInput = $("#fontSizeInput").val();
+      let letterSpacingInput = $("#letterSpacingInput").val();
+      let lineHeightInput = $("#lineHeightInput").val();
+      $('#capture').css('fontSize', fontSizeInput + 'px');
+      $('#capture').css('letterSpacing', letterSpacingInput + 'px');
+      $('#capture').css('lineHeight', lineHeightInput);
     });
   }
 });
 
 $('#btnScreenShot').click()
-
-
 
 var btn = document.getElementById("btnScreenShot");
 btn.addEventListener("mousedown", onScreenShotClick);
